c79383e6066a6557cd5a1a0616939e91a32d02fb
Chisom Esele <esele001@umn.edu>
2018-09-25 18:05:52 -0500

    Exercise #8

--- Homework/Exercise_8.cpp
+++ Homework/Exercise_8.cpp
@@ -31,27 +31,27 @@ void objectArea::print()
 // constructor 2
 int objectArea::rect(int length)
 {
-   cout << "This is the default constructor under test"<< endl;
-   cout << "Area printed by default constructor is: ";
-   c_length = length;
-   c_width = 3;
-   return length * c_width;
+    cout << "This is the default constructor under test" << endl;
+    cout << "Area printed by default constructor is: ";
+    c_length = length;
+    c_width = 3;
+    return length * c_width;
 }
 int main()
 {
-	//class object
+    //class object
     objectArea Rectangle;
-	
-	//class object for default constructor
+
+    //class object for default constructor
     objectArea defaultConstructorTest;
 
     cout << "Area of the Rectangle is " << Rectangle.rect(10, 3) << endl;
-    cout << "The Perimeter of the Rectangle is " << Rectangle.Peri(10, 3) <<endl;
+    cout << "The Perimeter of the Rectangle is " << Rectangle.Peri(10, 3) << endl;
     cout << '\n';
     cout << "Test print to see if my constructor is working" << endl;
     Rectangle.print();
-    cout <<'\n';
-    cout <<defaultConstructorTest.rect(10);
-    
+    cout << '\n';
+    cout << defaultConstructorTest.rect(10);
+
     return 0;
 }
