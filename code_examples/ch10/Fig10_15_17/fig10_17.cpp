aaa04c56bce9923e63a7bbd67954acb168de96bc
Chisom Esele <esele001@umn.edu>
2018-10-06 16:35:25 -0500

    update

--- code_examples/ch10/Fig10_15_17/fig10_17.cpp
+++ code_examples/ch10/Fig10_15_17/fig10_17.cpp
@@ -6,23 +6,23 @@ using namespace std;
 
 int main()
 {
-   Time t; // create Time object
+    Time t; // create Time object
 
-   // cascaded function calls
-   t.setHour( 18 ).setMinute( 30 ).setSecond( 22 );
+    // cascaded function calls
+    t.setHour(20).setMinute(30).setSecond(22);
 
-   // output time in universal and standard formats
-   cout << "Universal time: ";
-   t.printUniversal();
+    // output time in universal and standard formats
+    cout << "Universal time: ";
+    t.printUniversal();
 
-   cout << "\nStandard time: ";
-   t.printStandard();
+    cout << "\nStandard time: ";
+    t.printStandard();
 
-   cout << "\n\nNew standard time: ";
+    cout << "\n\nNew standard time: ";
 
-   // cascaded function calls
-   t.setTime( 20, 20, 20 ).printStandard();
-   cout << endl;
+    // cascaded function calls
+    t.setTime(20, 20, 20).printStandard();
+    cout << endl;
 } // end main
 
 /**************************************************************************
