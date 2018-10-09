ac4467243253723fda244aa8370d38023a352d1b
Chisom Esele <esele001@umn.edu>
2018-10-08 21:10:41 -0500

    textbook

--- C++_by_Ulris/Book.src/chap07/ball1.cpp
+++ C++_by_Ulris/Book.src/chap07/ball1.cpp
@@ -4,49 +4,56 @@
 //  ----------------------------------------------------
 //  To activate the control characters, a corresponding
 //  screen driver has to be loaded.
-//  For Windows 9x this is done by entering the following  
+//  For Windows 9x this is done by entering the following
 //  line contained in the file CONFIG.SYS:
-// 
+//
 //         DEVICE=C:\Windows\Command\Ansi.sys
-// ----------------------------------------------------- 
+// -----------------------------------------------------
 #include <iostream>
 #include <string>
 using namespace std;
 
-#define DELAY  10000000              // Output delay 
+#define DELAY 10000000 // Output delay
 
-#define CLS   (cout << "\033[2J")    // Clear sceen
+#define CLS (cout << "\033[2J") // Clear sceen
 
-#define LOCATE(r,c) (cout <<"\033["<< r <<';'<< c <<'H')
-        // Position the cursor in row r and column c
+#define LOCATE(r, c) (cout << "\033[" << r << ';' << c << 'H')
+// Position the cursor in row r and column c
 
-void main()
+int main()
 {
-   int x = 2, y = 3, dx = 1, speed = 0;
-   string floor(79, '-'),
-          header = "****  JUMPING BALL  ****";
+  int x = 2, y = 3, dx = 1, speed = 0;
+  string floor(79, '-'),
+      header = "****  JUMPING BALL  ****";
 
-   CLS;
-   LOCATE(1,25);  cout << header;
-   LOCATE(25,1);  cout << floor;
+  CLS;
+  LOCATE(1, 25);
+  cout << header;
+  LOCATE(25, 1);
+  cout << floor;
 
-   while(true)      // Let the ball "always" bounce
-   {                // Terminate by interrupt key (^C)
+  while (true) // Let the ball "always" bounce
+  {            // Terminate by interrupt key (^C)
 
-     LOCATE(y,x);  cout << 'o';        // Show the ball
-     for( long wait = 0; wait < DELAY; ++wait)
-       ;
+    LOCATE(y, x);
+    cout << 'o'; // Show the ball
+    for (long wait = 0; wait < DELAY; ++wait)
+      ;
 
-     if(x == 1 || x == 79) dx = -dx;   // Bounce off a wall?
+    if (x == 1 || x == 79)
+      dx = -dx; // Bounce off a wall?
 
-     if( y == 24 )                     // On the floor?
-     {
-       speed = - speed;
-       if( speed == 0 ) speed = -7;    // Restart
-     }
-     speed += 1;                  // Acceleration = 1
+    if (y == 24) // On the floor?
+    {
+      speed = -speed;
+      if (speed == 0)
+        speed = -7; // Restart
+    }
+    speed += 1; // Acceleration = 1
 
-     LOCATE(y,x); cout <<  ' ';   // Clear output
-     y += speed;  x += dx;        // New Position
-   }
+    LOCATE(y, x);
+    cout << ' '; // Clear output
+    y += speed;
+    x += dx; // New Position
+  }
 }
