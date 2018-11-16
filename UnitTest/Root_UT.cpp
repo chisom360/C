607721780ce321eaaba6a8a39c609ff0c0a8237a
Chisom Esele <esele001@umn.edu>
2018-11-16 00:29:16 -0600

    update

--- UnitTest/Root_UT.cpp
+++ UnitTest/Root_UT.cpp
@@ -1,8 +1,8 @@
 // tests.cpp
-#include "SquareRoot.cpp"
+#include "Root.cpp"
 #include <gtest/gtest.h>
 
-TEST(SquareRootTest, PositiveNos)
+TEST(RootTest, PositiveNos)
 {
     ASSERT_EQ(6, squareRoot(36.0));
     ASSERT_EQ(18.0, squareRoot(324.0));
@@ -10,7 +10,7 @@ TEST(SquareRootTest, PositiveNos)
     ASSERT_EQ(0, squareRoot(0.0));
 }
 
-TEST(SquareRootTest, NegativeNos)
+TEST(RootTest, NegativeNos)
 {
     ASSERT_EQ(-1.0, squareRoot(-15.0));
     ASSERT_EQ(-1.0, squareRoot(-0.2));
---
+++ UnitTest/build/CMakeFiles/SquareRoot_UT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SquareRoot_UT.dir/SquareRoot_UT.cpp.o"
+  "UnitTestBin/SquareRoot_UT.pdb"
+  "UnitTestBin/SquareRoot_UT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SquareRoot_UT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
