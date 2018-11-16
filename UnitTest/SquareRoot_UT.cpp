cd7284b81f4cc9169d55b9d7175024ed90af0518
Chisom Esele <esele001@umn.edu>
2018-11-15 21:52:21 -0600

    deleted

--- UnitTest/SquareRoot_UT.cpp
+++ UnitTest/SquareRoot_UT.cpp
@@ -16,8 +16,11 @@ TEST(SquareRootTest, NegativeNos)
     ASSERT_EQ(-1.0, squareRoot(-0.2));
 }
 
+/*
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+*/
--- UnitTest/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bigshow360/Documents/C++/UnitTest
-# Build directory: /home/bigshow360/Documents/C++/UnitTest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
