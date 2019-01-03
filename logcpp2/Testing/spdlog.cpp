e775cb1fcb5cec1b65d11c973ba9a65e89f3232a
Chisom Esele <esele001@umn.edu>
2019-01-02 22:48:19 -0600

    update

--- logcpp2/Testing/spdlog.cpp
+++ logcpp2/Testing/spdlog.cpp
@@ -3,6 +3,52 @@
 #include "spdlog/sinks/stdout_color_sinks.h" //support for stdout logging
 #include "spdlog/sinks/basic_file_sink.h"    // support for basic file logging
 #include "spdlog/sinks/rotating_file_sink.h" // support for rotating file logging
+void stdout_example()
+{
+    // create color multi threaded logger
+    auto console = spdlog::stdout_color_mt("console");
+    console->info("Welcome to spdlog!");
+    console->error("Some error message with arg: {}", 1);
+
+    auto err_logger = spdlog::stderr_color_mt("stderr");
+    err_logger->error("Some error message");
+
+    // Formatting examples
+    console->warn("Easy padding in numbers like {:08d}", 12);
+    console->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    console->info("Support for floats {:03.2f}", 1.23456);
+    console->info("Positional args are {1} {0}..", "too", "supported");
+    console->info("{:<30}", "left aligned");
+
+    spdlog::get("console")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
+
+    // Runtime log levels
+    spdlog::set_level(spdlog::level::info); // Set global log level to info
+    console->debug("This message should not be displayed!");
+    console->set_level(spdlog::level::trace); // Set specific logger's log level
+    console->debug("This message should be displayed..");
+
+    // Customize msg format for all loggers
+    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+    console->info("This an info message with custom format");
+
+    // Compile time log levels
+    // define SPDLOG_DEBUG_ON or SPDLOG_TRACE_ON
+    SPDLOG_TRACE(console, "Enabled only #ifdef SPDLOG_TRACE_ON..{} ,{}", 1, 3.23);
+    SPDLOG_DEBUG(console, "Enabled only #ifdef SPDLOG_DEBUG_ON.. {} ,{}", 1, 3.23);
+}
+
+int main()
+{
+    stdout_example();
+}
+
+/*
+#include <iostream>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h" //support for stdout logging
+#include "spdlog/sinks/basic_file_sink.h"    // support for basic file logging
+#include "spdlog/sinks/rotating_file_sink.h" // support for rotating file logging
 
 int main(int, char *[])
 {
@@ -11,7 +57,8 @@ int main(int, char *[])
     console->info("Welcome to spdlog!");
     console->info("An info message example {}..", 1);
     console->error("This is unacceptable");
-    console->warn("This is unacceptable");
+    console->warn("Hello {} {} !!", "param1", 123.4);
+    console->warn("int: {0:d};  hex: {0:#x};  oct: {0:#o};  bin: {0:#b}", 42);
 
     try
     {
@@ -26,3 +73,4 @@ int main(int, char *[])
         std::cout << "Log initialization failed: " << ex.what() << std::endl;
     }
 }
+*/
