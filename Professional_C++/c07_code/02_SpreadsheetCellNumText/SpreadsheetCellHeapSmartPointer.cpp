13e560bf5cff6150c39a661b213358dfd146fdcc
Chisom Esele <esele001@umn.edu>
2019-05-27 19:37:17 -0500

    run

--- Professional_C++/c07_code/02_SpreadsheetCellNumText/SpreadsheetCellHeapSmartPointer.cpp
+++ Professional_C++/c07_code/02_SpreadsheetCellNumText/SpreadsheetCellHeapSmartPointer.cpp
@@ -8,10 +8,9 @@ int main()
 	auto myCellp = make_unique<SpreadsheetCell>();
 	// Equivalent to:
 	// unique_ptr<SpreadsheetCell> myCellp(new SpreadsheetCell());
-	
+
 	myCellp->setValue(3.7);
-	cout << "cell 1: " << myCellp->getValue() <<
-		" " << myCellp->getString() << endl;
+	cout << "cell 1: " << myCellp->getValue() << " " << myCellp->getString() << endl;
 
 	return 0;
 }
--- Professional_C++/c07_code/02_SpreadsheetCellNumText/SpreadsheetCellTest.cpp
+++ Professional_C++/c07_code/02_SpreadsheetCellNumText/SpreadsheetCellTest.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
 	SpreadsheetCell myCell, anotherCell;
-	myCell.setValue(6);
-	anotherCell.setString("3.2");
+	myCell.setValue(3);
+	anotherCell.setString("1");
 
 	cout << "cell 1: " << myCell.getValue() << endl;
 	cout << "cell 2: " << anotherCell.getValue() << endl;
